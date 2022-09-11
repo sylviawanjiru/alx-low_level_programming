f71d1667c5bc576076b353654559cc3338faf099
sylviawanjiru <sylviawanjiru>
2022-09-11 06:14:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,16 @@
 #include <unistd.h>
 
 /**
+ * Main - prints a text
  *
- * * Main - prints a text
- *
- * * Return: Always 1
- *
- * */
+ * Return: Always 1
+ */
 
 int main(void)
 
 {
 
-	         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-		           return (1);
-
+	 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	 return (1);
+	
 }
