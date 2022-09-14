3d2ef16baef639f7c96738875dfeaa0ce30e91f0
sylviawanjiru <sylviawanjiru>
2022-09-14 13:40:38 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include "main h"
 
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * return: Always 0
  */
-int main(void)
+Return: Always 0
 {
-	putchar('H');
-	putchar('i');
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+	_putchar('a');
+        _putchar('r');
+        _putchar('\n')	
 
 return (0);
+
 }
