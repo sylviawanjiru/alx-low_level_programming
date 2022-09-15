cd1a153bc4cb2e0e7015b2cab30a4ef81749195a
sylviawanjiru <sylviawanjiru>
2022-09-15 12:28:13 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main h"
+#include "main.h"
 /**
  * main - entry point
  * program description
@@ -17,7 +17,7 @@ int main(void)
         _putchar('h');
 	_putchar('a');
         _putchar('r');
-        _putchar('\n')	
+        _putchar('\n');	
 
 return (0);
 }
