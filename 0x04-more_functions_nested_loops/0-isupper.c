9188ce4ea99d9633eb0c35f00bd1f6b54821e0fc
sylviawanjiru <sylviawanjiru>
2022-09-15 19:09:32 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,27 +6,20 @@
  * @c: character to be tested
  *
  * Return: 1 whether it is, 0 otherwise
- *
- * */
-
-
+ */
 
 int _isupper(int c)
 
 {
 
-		if ((c >= 'A') && (c <= 'Z'))
+if ((c >= 'A') && (c <= 'Z'))
 
-				{
+{
 
-							return (1);
+return (1);
 
-								}
+}
 
-			else
-
-
-
-					return (0);
+else
 
 }
