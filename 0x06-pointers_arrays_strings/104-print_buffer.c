8683a8fc176cd257b651a25f14b99526d3862992
sylviawanjiru <sylviawanjiru>
2022-09-23 00:04:39 -0700

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 
+#include "main.h"
+#include <stdio.h>
+
 /**
  * isPrintableASCII - determines if n is a printable ASCII char
  * @n: integer
@@ -8,7 +11,7 @@
  */
 int isPrintableASCII(int n)
 {
-return (n >= 32 && n <= 126);
+	return (n >= 32 && n <= 126);
 }
 
 /**
@@ -17,22 +20,20 @@ return (n >= 32 && n <= 126);
  * @start: starting position
  * @end: ending position
  */
-
 void printHexes(char *b, int start, int end)
 {
-int i = 0;
-{
+	int i = 0;
 
-while (i < 10)
-{
-if (i < end)
-printf("%02x", *(b + start + i));
-else
-printf("  ");
-if (i % 2)
-printf(" ");
-i++;
-}
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
 }
 
 /**
@@ -42,19 +43,18 @@ i++;
  * @start: starting position
  * @end: ending position
  */
-
 void printASCII(char *b, int start, int end)
 {
-int ch, i = 0;
+	int ch, i = 0;
 
-while (i < end)
-{
-ch = *(b + i + start);
-if (!isPrintableASCII(ch))
-ch = 46;
-printf("%c", ch);
-i++;
-}
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
 }
 
 /**
@@ -62,21 +62,21 @@ i++;
  * @b: string
  * @size: size of buffer
  */
-
 void print_buffer(char *b, int size)
 {
-int start, end;
+	int start, end;
 
-if (size > 0)
-{
-for (start = 0; start < size; start += 10)
-{
-end = (size - start < 10) ? size - start : 10;
-printf("%08x: ", start);
-printHexes(b, start, end);
-printASCII(b, start, end);
-printf("\n");
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	} else
+		printf("\n");
 }
-}else
-printf("\n");
-}
+
