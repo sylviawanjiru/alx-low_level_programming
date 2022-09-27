50587bb484b2a70c09d083c255bc6839170e7912
sylviawanjiru <sylvia.shiross@gmail.com>
2022-09-27 06:46:05 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@
  *  int prototypes()
  */
 
+#include <stddef.h>
+
 int _putchar(char c);                                                                                                            
 void *_memset(void *s, int c, size_t n);                                                                                         char *_memcpy(char *dest, char *src, unsigned int n);                                                                          
 char *_strchr(char *s, char c);                                                                                                 
