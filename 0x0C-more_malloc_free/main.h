015f7115823aeb0735c7b0571ce293ea88143ffd
sylviawanjiru <sylvia.shiross@gmail.som>
2022-10-07 08:33:43 -0700

    main h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
