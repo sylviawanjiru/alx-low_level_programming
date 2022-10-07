e4dae8a84c625119ee9e273dc45a70c76eaf02fb
sylviawanjiru <sylvia.shiross@gmail.som>
2022-10-07 09:29:31 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,19 +1,47 @@
 #include "main.h"
-/**
- * free_grid - allocates a grid, make space and free space
- * @grid: takes in width of grid
- * @height: height of grid
- * Return: free grid
- */
-void free_grid(int **grid, int height)
-{
-	int i;
+#include <stdlib.h>
 
-	for (i = 0; i < height; i++)
+/**
+ * _realloc - Reallocates a block of memory to expand it
+ * @ptr: Previous memory pointer
+ * @old_size: The former size of the memory
+ * @new_size: The new size to be allocated to this memory
+ *
+ * Return: Void Pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
 	{
-		free(grid[i]);
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+		return (nptr);
 	}
 
-	free(grid);
-}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+	{
+		nptr[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+	return (nptr);
+
+
+}
